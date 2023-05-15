5a86d66af6020c708b355d454d07638ff77974f4
kkangjee <wlgns337@gmail.com>
2023-05-15 16:52:14 +0900

    초기 설정 20230515

---
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.androidlab
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 화면출력
+        //setContentView(R.layout.activity_main)
+
+        // xml 대신 코드로 작성하기(비효율적)
+        val button = Button(this)
+        button.text = "헬로~"
+        setContentView(button)
+    }
+}
